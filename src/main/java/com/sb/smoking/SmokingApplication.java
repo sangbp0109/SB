77cce8b6f919e6bab4aab02d15249ed85079fae0
SPARK <realzidane05@naver.com>
2022-01-11 18:39:06 +0900

    22/01/12 00:18

---
+++ src/main/java/com/sb/smoking/SmokingApplication.java
@@ -0,0 +1,13 @@
+package com.sb.smoking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmokingApplication.class, args);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/views/join/222.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user1
+  Date: 2022-01-11
+  Time: 오후 5:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
